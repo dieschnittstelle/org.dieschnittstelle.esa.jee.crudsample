2e25afe95c6c144427df12b0ed750bafb701e020
jk <joern.kreutel@beuth-hochschule.de>
2016-06-17 23:09:39 +0200

    opimise packaging in ear allow to switch between hibernate and mdb from outside

--- org.dieschnittstelle.jee.esa.crudsample.ejbmodule/src/main/java/org/dieschnittstelle/jee/esa/skeleton/ejb/TouchpointCRUDServiceImpl.java
+++ org.dieschnittstelle.jee.esa.crudsample.ejbmodule/src/main/java/org/dieschnittstelle/jee/esa/skeleton/ejb/TouchpointCRUDServiceImpl.java
@@ -4,6 +4,9 @@ import org.dieschnittstelle.jee.esa.entities.crm.StationaryTouchpoint;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.Context;
+import org.apache.log4j.Logger;
 
 /**
  * Created by master on 07.06.16.
@@ -11,21 +14,49 @@ import javax.ejb.Stateless;
 @Stateless
 public class TouchpointCRUDServiceImpl implements TouchpointCRUDService {
 
+    protected static Logger logger = Logger.getLogger(TouchpointCRUDServiceImpl.class);
+
     @EJB(beanName = "TouchpointCRUDStatelessMDB")
+    private TouchpointCRUDLocal crudejbMDB;
+
+    @EJB(beanName = "TouchpointCRUDStateless")
     private TouchpointCRUDLocal crudejb;
+
+    @Context
+    private HttpServletRequest request;
 
     @Override
     public CRUDResult<StationaryTouchpoint> createTouchpoint(StationaryTouchpoint tp) {
-        return crudejb.createTouchpoint(tp);
+        String crudprovider = request.getParameter("crudprovider");
+        logger.info("crudprovider: " + crudprovider);
+        // determine the crudprovider
+        if ("TouchpointCRUDStatelessMDB".equals(crudprovider)) {
+            return crudejbMDB.createTouchpoint(tp);
+        }
+        else {
+            return crudejb.createTouchpoint(tp);
+        }
     }
 
     @Override
     public CRUDResult<StationaryTouchpoint> readAllTouchpoints() {
-        return crudejb.readAllTouchpoints();
+        String crudprovider = request.getParameter("crudprovider");
+        if ("TouchpointCRUDStatelessMDB".equals(crudprovider)) {
+            return crudejbMDB.readAllTouchpoints();
+        }
+        else {
+            return crudejb.readAllTouchpoints();
+        }
     }
 
     @Override
     public CRUDResult<StationaryTouchpoint> readTouchpoint(String id) {
-        return crudejb.readTouchpoint(id);
+        String crudprovider = request.getParameter("crudprovider");
+        if ("TouchpointCRUDStatelessMDB".equals(crudprovider)) {
+            return crudejbMDB.readTouchpoint(id);
+        }
+        else {
+            return crudejb.readTouchpoint(id);
+        }
     }
 }
